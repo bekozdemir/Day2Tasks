c3f5ad76f19b9a201fe4a2d9445f50dd0ef10713
Berkay <brkyzdmr@gmail.com>
2021-04-30 13:39:13 +0300

    day2tasks

--- src/Category.java
+++ src/Category.java
@@ -7,4 +7,20 @@ public class Category {
 		this.id = id;
 		this.categoryName = categoryName;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
 }
